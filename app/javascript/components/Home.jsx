f9cb947743e510c398f5a1e72348d6622cf7bc8a
gabrieldominguezduran <gabrieldominguezduran@gmail.com>
2021-09-16 11:39:28 +0200

    Initial commit

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Home = () => (
+  <div className="container d-flex justify-content-center">
+    <h1 className="m-5">My Blog</h1>
+  </div>
+)
+
+export default Home
